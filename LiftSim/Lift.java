4b269324d50e7f031efada6d574ac5113580423b
Felix <gloin@edoras>
2015-06-30 14:34:27 +0200

    Backup. Try fix Exception in thread main java.lang.NullPointerException

--- LiftSim/Lift.java
+++ LiftSim/Lift.java
@@ -5,11 +5,14 @@ public class Lift
 	private Etage[]    etage;
 	private int        anzEtagen;
 	private Wuensche[] wuensche;
+
 	public Lift( int anzEtagen)
 	{
-		this.anzEtagen = anzEtagen;
 		if (anzEtagen<2)
 			return;
+		this.etage = new Etage[anzEtagen-1];
+		this.anzEtagen = anzEtagen;
+
 		this.etage[0] = new Etage(0,-1, "Unten");
 
 		for(int i=1; i<anzEtagen-1; i++)
@@ -18,7 +21,7 @@ public class Lift
 		}
 		this.etage[anzEtagen-1] = new Etage(anzEtagen-1, 1, "Oben");
 	}
-	public int[] getEtagenStatus(int enr)
+	public int[] getEtagenStatus()
 	{
 		int[] s = new int[this.anzEtagen];
 		for(int i = 0; i<this.anzEtagen; i++){
--- LiftSim/Simulation.java
+++ LiftSim/Simulation.java
@@ -3,12 +3,19 @@ package LiftSim;
 public class Simulation
 {
 	private Lift lift;
+	private int  anzEtagen;
 	public Simulation(int anzEtagen)
 	{
 		  this.lift = new Lift(anzEtagen);
+		  this.anzEtagen = anzEtagen;
 	}
 	public void printStatus()
 	{
-		int[] etagenStatus = this.lift.getEtagenStatus();
+		int[] etagenStatus;
+		//= new int[this.anzEtagen];
+		etagenStatus = this.lift.getEtagenStatus();
+		for (int i=this.anzEtagen-1; i>=0; i--){
+			System.out.println("Etage "+i+" Status: "+etagenStatus[i]);
+		}
 	}
 }
