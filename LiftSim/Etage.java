8f90403fc9ce2260bee9fb3ea1addcab54529433
Felix <felix_gerling@online.de>
2015-07-01 10:44:10 +0200

    Class Etage ins Englische übersetzt und kommentiert.

--- LiftSim/Etage.java
+++ LiftSim/Etage.java
@@ -2,56 +2,90 @@ package LiftSim;
 
 public class Etage
 {
-   /** ruf[0] runter
-    *  ruf[1] hoch
+   /**
+	* call[0] runter
+    * call[1] hoch
     */
-	private boolean[] ruf;
+	private boolean[] call;
 
 	private int       position;
 
-	/* 1 = ganz oben, 0 = mitte, -1 ganz unten
+	/**
+	 * Wo befindet sich die Etage? Ganz Oben? Unten? Dazwischen? 
+	 * Wäre nicht nötig wenn man eine Liste verwenden würden. 
+	 *  1 = ganz oben
+	 *  0 = mitte
+	 * -1 ganz unten
 	 */
 	private int ext;
 
-
-	/** Beschreibung der Etage z.b.: Ergdeschoß, Keller...
+	/**
+	 * Beschreibung der Etage z.b.: Ergdeschoß, Keller...
 	 */
-	private String beschr;            
-	public Etage(int pos, int ext, String beschr)
+	private String description;            
+
+	/**
+	 * Konstruktor
+	 * @param pos         Position der Etage
+	 * @param ext         Relation der Etage zur anderen
+	 * @param description Lesbare Etagen Beschreibung
+	 */
+	public Etage(int pos, int ext, String description)
 	{
-		this.ruf  = new boolean[2];
-		this.ruf[0] = false;
-		this.ruf[1] = false;
-		this.ext    = ext;
-		this.position    = pos;
-		this.beschr = beschr;
+		this.call  = new boolean[2];
+
+		this.call[0]        = false;
+		this.call[1]        = false;
+
+		this.ext           = ext;
+		this.position      = pos;
+		this.description   = description;
 	}
+
+	/**
+	 * Setzt den Ruf für die gewünschte Richtung auf dieser Etage.
+	 * @param direction Richtung die "gedrückt" werden soll. 0 = Runter; 1 = Hoch;
+	 * @todo Konstanten für Hoch und Runter verwenden
+	 */
 	public void setCall(int direction)
 	{
-		this.ruf[direction] = true;
+		this.call[direction] = true;
 	}
-	public void entfRuf(int richtung)
+
+	/**
+	 * löscht den "gedrückt" status der gewünschten Richtung. 
+	 * @param direction Richtung die "gelöscht" werden soll. 0 = Runter; 1 = Hoch;
+	 */
+	public void delCall(int direction)
 	{
-		this.ruf[richtung] = false;
+		this.call[direction] = false;
 	}
-	/** down  up  return
-	 * 	0     0   = 0
-	 * 	0     1   = 1
-	 * 	1     0   = 2
-	 * 	1     1   = 3
+
+	/**
+	 * Gibt den aktuellen Rufstatus zurück.
+	 *
+	 * down  up  return
+	 * 0     0   = 0
+	 * 0     1   = 1
+	 * 1     0   = 2
+	 * 1     1   = 3
 	 *
 	 * 	@return 0 = no call; 1 = up; 2 = down; 3 = both;
 	 */
 	public int getCall()
 	{
 		int call = 0;
-		call += this.ruf[0] ? 2 : 0; // runter
-		call += this.ruf[1] ? 1 : 0; // hoch
+		call += this.call[0] ? 2 : 0; // runter
+		call += this.call[1] ? 1 : 0; // hoch
 		return call;
 	}
+	/**
+	 * Wird aufgerufen wenn eine Instanz dieser Klasse wie ein String behandelt wird.
+	 * Bsp.: System.out.println(etage);
+	 */
 	public String toString()
 	{
-		return this.beschr;
+		return this.description;
 	}
 }
 
